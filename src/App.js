d90a2489dcffc83142efc0a2fbe1929d04e767b7
Katy <78986968+Kateryna-99@users.noreply.github.com>
2021-10-02 00:40:16 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import './App.css';
 
+
 export default function App() {
+  const someList= [
+    "Animals",
+    "Animals",
+    "Anime",
+     "Anti-Malware",
+     "Art Design",
+    "Books",
+    "Business",
+    "Calendar",
+     "Cloud Storage",
+     "Continuous Integration",
+     "Cryptocurrency",
+     "File Sharing",
+    ];
+ function getKey(str){
+ for (let i=0; i< str.length; i++){
+ key +=str.charCodeAt(i);
+}  
+return key.toStping() ;
+}
   return (
     <div>
-      Some List:
+    <ul>
+    {someList.map(el=> 
+     <li key={getKey(el)}>{el.item}</li>
+    )}
+</ul>
     </div>
   );
 }
