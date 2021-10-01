d5f8a21675b8eb2423dbfe96497243e59fdeafd0
Katy <78986968+Kateryna-99@users.noreply.github.com>
2021-10-01 23:30:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+mport React from 'react';
 import './App.css';
+let array = [
+    "Animals",
+    "Animals",
+    "Anime",
+   "Anti-Malware",
+   "Art Design",
+     "Books",
+     "Business",
+     "Calendar",
+    "Cloud Storage",
+     "Continuous Integration",
+     "Cryptocurrency",
+     "File Sharing",
+   ]
 
-export default function App() {
+export default function App(array) {
+              
   return (
-    <div>
-      Some List:
-    </div>
+    <ul>
+    {array.map(item=> (
+     <li key={item.id}>{item.text}</li>
+    )
+    )
+  }
+</ul>
+    
   );
 }
+
