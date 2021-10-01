3f080f2b82322184757cfbd9139d8580cb05cbbe
Katy <78986968+Kateryna-99@users.noreply.github.com>
2021-10-01 23:04:37 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
+  let someList=[
+   {id: 1, item: 'Animals'},
+{ id: 2, item: 'Anime'},
+{ id: 3, item: 'Anti-Malware'},
+{ id: 4, item: 'Art Design'},
+{ id: 5, item: 'Books'},
+{ id: 6, item: 'Business'},
+{ id: 7, item: 'Calendar'},
+{ id: 8, item: 'Cloud Storage'},
+{ id: 9, item: 'File Sharing'},
+{ id: 10, item: 'Animals'},
+{ id: 11, item: 'Continuous Integration'},
+{ id: 12, item: 'Cryptocurrency'},
+    ];            
   return (
     <div>
-      Some List:
+    <ul>
+    {someList.map(el=> (
+     <li key={el.id}>{el.item}</li>
+    )
+    )
+  }
+</ul>
     </div>
   );
 }
