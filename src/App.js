e0e3dc07062afd9ea3d656767d6c536fcc7afe1d
Katy <lukjanchenko85@gnail.com>
2021-10-01 23:56:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import React from 'react';
 import './App.css';
+let array = [
+    "Animals",
+    "Animals",
+    "Anime",
+   "Anti-Malware",
+   "Art Design",
+     "Books",
+     "Business",
+     "Calendar",
+    "Cloud Storage",
+     "Continuous Integration",
+     "Cryptocurrency",
+     "File Sharing",
+   ]
 
-export default function App() {
-  let someList=[
-   {id: 1, item: 'Animals'},
-{ id: 2, item: 'Anime'},
-{ id: 3, item: 'Anti-Malware'},
-{ id: 4, item: 'Art Design'},
-{ id: 5, item: 'Books'},
-{ id: 6, item: 'Business'},
-{ id: 7, item: 'Calendar'},
-{ id: 8, item: 'Cloud Storage'},
-{ id: 9, item: 'File Sharing'},
-{ id: 10, item: 'Animals'},
-{ id: 11, item: 'Continuous Integration'},
-{ id: 12, item: 'Cryptocurrency'},
-    ];            
+export default function App(props) {
+     const  
   return (
-    <div>
     <ul>
-    {someList.map(el=> (
-     <li key={el.id}>{el.item}</li>
+    {array.map(item=> (
+     <li key={item.id}>{item.text}</li>
     )
     )
   }
 </ul>
-    </div>
+    
+  );
+}
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <ListItem key={number.toString()}
+              value={number} />
+
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
   );
 }
